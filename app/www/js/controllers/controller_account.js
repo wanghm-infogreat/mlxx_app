28107106a46b08bd8a27f11950ef978fbc645fe2
wanghm0301 <macmini_yh@126.com>
2016-12-29 13:55:21 +0800

    Add files via upload

--- app/www/js/controllers/controller_account.js
+++ app/www/js/controllers/controller_account.js
@@ -1,14 +1,18 @@
 angular.module('starter.controller.account', [])
 
-.controller('AccountCtrl', function($scope, $state, $cordovaToast, Account) {
+.controller('AccountCtrl', function($scope, $state, $cordovaToast, md5, Account) {
 
+  /**********************************************************/
   // 初始化登录页面
+  /**********************************************************/
   $scope.account = {
     user: '',
     pass: ''
   };
 
+  /**********************************************************/
   // 登录按钮按下
+  /**********************************************************/
   $scope.login = function() {
 
       // 验证用户名和密码是否为空白
@@ -16,8 +20,28 @@ angular.module('starter.controller.account', [])
         $cordovaToast.show('请输入用户名和密码。', 'short', 'center');
         return;
       }
-      
+
+	  // 用户名
+	  var user = $scope.account.user;
+      // 加密密码
+      var pass = md5.createHash($scope.account.pass);
+	  
       // 登录
-      Account.login($scope, $scope.account.user, $scope.account.pass);
+      Account.login(user, pass).then(function(data) {
+
+		// 保存当前用户的用户名
+   	    localStorage.setItem("name", data.name);
+      	// 保存当前用户的user
+        localStorage.setItem("user", JSON.stringify(data.user));
+        // 保存当前用户的token
+   	    localStorage.setItem("token", data.token);
+              
+        // 清除用户名和密码
+        $scope.account.user = "";
+        $scope.account.pass = "";
+
+        // 返回成功
+        $state.go('tab.dash');
+  	 });
   };
 });
--- app/www/js/controllers/controller_bbs.js
+++ app/www/js/controllers/controller_bbs.js
@@ -5,22 +5,86 @@ angular.module('starter.controller.bbs', [])
  */
 .controller('BbsGroupCtrl', function($scope, $rootScope, BbsGroup) {
   
-  // 初始化论坛组列表
-  $scope.groups = {};
+  /**********************************************************/
+  // 画面初始化
+  /**********************************************************/
   // 取得论坛组列表
-  BbsGroup.list($scope);
+  BbsGroup.list().then(function(groups) {
+	// 设定画面的论坛组信息
+  	$scope.groups = groups;
+  });
   
+  /**********************************************************/
+  // 刷新
+  /**********************************************************/
+  $scope.doRefresh = function() {
+    // 初始化
+    BbsGroup.init();
+    
+    // 刷新画面内容
+    BbsGroup.list().then(function(groups) {
+
+      // 设定画面的论坛组信息
+      $scope.groups = groups;
+      
+      // 关闭画面刷新动画效果
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  /**********************************************************/
   // 关注论坛组
+  /**********************************************************/
   $scope.unfavority = function(group) {
-    BbsGroup.unfavority($scope, group);
+  	// 关注论坛组
+    BbsGroup.unfavority(group).then(function(data) {
+	
+        // 取得画面的所有关注对象
+        var favorites = $scope.groups.favorites;
+      
+        // 取得画面的所有未关注对象
+        var others = $scope.groups.others;
+      
+        // 将操作对象加入到未关注列表中
+        others.push(group);
+      
+        // 从关注对象中移除
+        for (var i = 0; i < favorites.length; i++) {
+          if (favorites[i].id === group.id) {
+            favorites.splice(i, 1);
+            break;
+          }
+        }
+	});
   }
 
+  /**********************************************************/
   // 取消关注论坛组
+  /**********************************************************/
   $scope.favority = function(group) {
-    BbsGroup.favority($scope, group);
+    BbsGroup.favority(group).then(function(data) {
+		// 取得画面的所有关注对象
+		var favorites = $scope.groups.favorites;
+      
+        // 取得画面的所有未关注对象
+        var others = $scope.groups.others;
+      
+        // 将操作对象加入到关注列表中
+        favorites.push(group);
+      
+        // 从未关注对象中移除
+        for (var i = 0; i < others.length; i++) {
+          if (others[i].id === group.id) {
+            others.splice(i, 1);
+            break;
+          }
+        }
+	});
   }
   
+  /**********************************************************/
   // 点击论坛组保存信息
+  /**********************************************************/
   $scope.saveGroup = function(group) {
     $rootScope.group = group;
   }
@@ -29,41 +93,89 @@ angular.module('starter.controller.bbs', [])
 /*
  * 论坛话题Ctrl
  */
-.controller('BbsTitleCtrl', function($rootScope, $scope, $stateParams, $ionicPopup, $rootScope, BbsTitle, BbsGroup) {
+.controller('BbsTitleCtrl', function(
+	$rootScope,
+	$scope,
+	$stateParams,
+	$ionicPopup,
+	$rootScope,
+	$ionicScrollDelegate,
+	BbsTitle,
+	BbsGroup) {
 
+  /**********************************************************/
+  // 画面初始化
+  /**********************************************************/
   // 初始化话题列表
-  $scope.titles = [];
-  // 取得话题列表
-  BbsTitle.list($scope, $stateParams.groupId);
+  BbsTitle.init();
+
   // 保存当前的论坛组id
+  var groupid = $stateParams.groupId;
   $rootScope.groupid = $stateParams.groupId;
 
+  // 取得话题列表
+  BbsTitle.list(groupid).then(function(titles) {
+	// 设定画面的话题信息
+  	$scope.titles = titles;
+  });
+
+  /**********************************************************/
   // 刷新
+  /**********************************************************/
   $scope.doRefresh = function() {
-    $scope.titles = [];
-    BbsTitle.list($scope, $rootScope.groupid);
-  };
-  
-  // 加载更多
-  $scope.loadMore = function() {
-    BbsTitle.list($scope, $rootScope.groupid);
-  };
-  
-  $scope.moreDataCanBeLoaded = function() {
-    return BbsTitle.hasMore();
+    // 初始化
+    BbsGroup.init();
+    
+    // 刷新画面内容
+    BbsGroup.list(groupid).then(function(groups) {
+
+      // 设定画面的论坛组信息
+      $scope.groups = groups;
+      
+      // 关闭画面刷新动画效果
+      $scope.$broadcast('scroll.refreshComplete');
+    });
   };
 
+  /**********************************************************/
+  // 加载更多
+  /**********************************************************/
+  $scope.doLoadMore = function() {
+    // 刷新画面内容
+    BbsTitle.list(groupid).then(function(groups) {
+
+      // 设定画面的论坛组信息
+      $scope.groups = groups;
+      
+      // 关闭画面加载动画效果
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+  
+  /**********************************************************/
+  // 能否加载更多数据控制
+  /**********************************************************/
+  $scope.hasMore = function() {
+    return BbsTitle.hasMore();
+  };
+  
+  /**********************************************************/
   // 点击话题保存信息
+  /**********************************************************/
   $scope.saveTitle = function(title) {
     $rootScope.title = title;
   }
 
+  /**********************************************************/
   // 收藏话题
+  /**********************************************************/
   $scope.favority = function(title) {
-    BbsTitle.favority($scope, title);
+    BbsTitle.favority(title);
   }
   
+  /**********************************************************/
   // 新建话题
+  /**********************************************************/
   $scope.doNewTitle = function() {
 
     // 显示输入框
@@ -78,7 +190,25 @@ angular.module('starter.controller.bbs', [])
       //
       if (res != undefined) {
         // 新建话题
-        BbsTitle.newTitle($scope, $rootScope.groupid, res, BbsGroup.getGroups());
+        BbsTitle.newTitle($rootScope.groupid, res).then(function (data) {
+
+			// 设定画面话题一览
+			$scope.titles = data.titles;
+
+			// 取得全部论坛组
+			var groups = BbsGroup.groups();
+
+            // 更新论坛组信息
+            for(var i = 0; i < groups.length; i++) {
+              if (groups[i].id == groupid) {
+                groups[i].titles = data.group.titles;
+                break;
+              }
+            }
+            
+            // 滚动到画面顶部
+            $ionicScrollDelegate.$getByHandle('scrollTitle').scrollTop();
+		});
       }
     });
   }
@@ -89,35 +219,79 @@ angular.module('starter.controller.bbs', [])
  */
 .controller('BbsFavoriteCtrl', function($scope, $stateParams, $rootScope, BbsTitle) {
 
-  // 初始化收藏的话题列表
-  $scope.title_favorites = [];
-  // 取得收藏的话题列表
-  BbsTitle.favorites($scope);
-  // 取消收藏话题
-  $scope.unfavority = function(title) {
-    BbsTitle.unfavority($scope, title);
-  }
-  
+  /**********************************************************/
+  // 画面初始化
+  /**********************************************************/
+  // 初始化话题列表
+  BbsTitle.init();
+
+  // 取得话题列表
+  BbsTitle.favorites().then(function(favorites) {
+	// 设定画面的话题信息
+  	$scope.favorites = favorites;
+  });
+
+  /**********************************************************/
   // 刷新
+  /**********************************************************/
   $scope.doRefresh = function() {
-    // 初始化收藏的话题列表
-    $scope.title_favorites = [];
-    // 取得收藏的话题列表
-    BbsTitle.favorites($scope);
-  };
-  
-  // 加载更多
-  $scope.loadMore = function() {
-    // 取得收藏的话题列表
-    BbsTitle.favorites($scope);
-  };
-  
-  // 是否还有更多数据
-  $scope.moreFavoCanBeLoaded = function() {
-    return BbsTitle.hasMoreFavorite();
+    // 初始化
+    BbsTitle.init();
+
+    // 刷新画面内容
+    BbsTitle.favorites().then(function(favorites) {
+
+	  // 设定画面的话题信息
+	  $scope.favorites = favorites;
+
+      // 关闭画面刷新动画效果
+      $scope.$broadcast('scroll.refreshComplete');
+    });
   };
 
+  /**********************************************************/
+  // 加载更多
+  /**********************************************************/
+  $scope.doLoadMore = function() {
+    // 刷新画面内容
+    BbsTitle.favorites().then(function(favorites) {
+
+	  // 设定画面的话题信息
+	  $scope.favorites = favorites;
+      
+      // 关闭画面加载动画效果
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+  
+  /**********************************************************/
+  // 能否加载更多数据控制
+  /**********************************************************/
+  $scope.hasMore = function() {
+    return BbsTitle.hasMore();
+  };
+  
+  /**********************************************************/
+  // 取消收藏话题
+  /**********************************************************/
+  $scope.unfavority = function(title) {
+    BbsTitle.unfavority(title).then(function(data) {
+      // 取得画面的所有收藏话题
+      var favorites = $scope.favorites;
+    
+      // 从收藏对象中移除
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].title.id == title.id) {
+          favorites.splice(i, 1);
+          break;
+        }
+      }
+	});
+  }
+  
+  /**********************************************************/
   // 点击话题保存信息
+  /**********************************************************/
   $scope.saveTitle = function(title) {
     $rootScope.title = title;
   }
@@ -126,40 +300,105 @@ angular.module('starter.controller.bbs', [])
 /**
  * 评论内容ctrl
  */
-.controller('BbsCommentCtrl', function($scope, $ionicScrollDelegate, $stateParams, BbsTitle, BbsComment) {
+.controller('BbsCommentCtrl', function(
+	$scope,
+	$ionicScrollDelegate,
+	$stateParams,
+	BbsTitle,
+	BbsComment) {
 
   // 隐藏内容
   $scope.loaded = false;
 
+  /**********************************************************/
+  // 画面初始化
+  /**********************************************************/
   // 初始化发表评论内容
   $scope.content = "";
-  // 初始化评论列表
-  $scope.comments = [];
+
+  // 初始化话题列表
+  BbsComment.init();
+
   // 取得评论列表
-  $scope.titleid = $stateParams.titleId;
-  BbsComment.comments($scope, $scope.titleid);
-  
+  var titleid = $stateParams.titleId;
+  $scope.titleid = titleid;
+  BbsComment.list(titleid).then(function(comments) {
+	// 设定画面的论列表
+  	$scope.comments = comments;
+  });
+
+  /**********************************************************/
   // 滚动到底部
+  /**********************************************************/
   $scope.$on('$ionicView.afterEnter', function() {
     $ionicScrollDelegate.$getByHandle('scrollComment').scrollBottom();
     $scope.loaded = true;
   });
 
+  /**********************************************************/
   // 刷新
+  /**********************************************************/
   $scope.doRefresh = function() {
-    $scope.comments = [];
-    BbsComment.comments($scope, $scope.titleid);
+    // 初始化
+    BbsComment.init();
+
+    // 刷新画面内容
+    BbsComment.list(titleid).then(function(comments) {
+
+	  // 设定画面的论列表
+	  $scope.comments = comments;
+
+      // 关闭画面加载动画效果
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
   };
-  
+
+  /**********************************************************/
   // 加载更多
-  $scope.loadMore = function() {
-    BbsComment.comments($scope, $scope.titleid);
+  /**********************************************************/
+  $scope.doLoadMore = function() {
+    // 刷新画面内容
+    BbsComment.list(titleid).then(function(comments) {
+
+	  // 设定画面的论列表
+	  $scope.comments = comments;
+      
+      // 关闭画面刷新动画效果
+      $scope.$broadcast('scroll.refreshComplete');
+    });
   };
   
+  /**********************************************************/
+  // 能否加载更多数据控制
+  /**********************************************************/
+  $scope.hasMore = function() {
+    return BbsComment.hasMore();
+  };
+
+  /**********************************************************/
   // 发布评论
+  /**********************************************************/
   $scope.doSend = function() {
     // 发表评论
-    BbsComment.send($scope, $scope.titleid, BbsTitle.getTitles());
+    BbsComment.comment(titleid, BbsTitle.titles(), $scope.content).then(function(data) {
+
+	  // 设定画面的论列表
+	  $scope.comments = data.comments;
+
+      // 清除画面的输入内容
+      $scope.content = "";
+
+      // 更新当前的话题信息
+	  titles = BbsTitle.titles();
+      for(var i = 0; i < titles.length; i++) {
+        if (titles[i].id == titleid) {
+          titles[i].comments = data.title.comments;
+          break;
+        }
+      }
+
+      // 滚动到画面底部
+      $ionicScrollDelegate.$getByHandle('scrollComment').scrollBottom();
+	});
   }
-  
 });
